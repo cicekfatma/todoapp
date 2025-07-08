0738f1725f67467c9c2a1321cab6aa8dcb2ac07a
Fatma Cicek <fatmacicek139@gmail.com>
2025-07-08 13:31:34 +0300

    Local Storage eklendi

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,17 +1,38 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common'; // <-- gerekli
-
+import { FormsModule } from '@angular/forms'; // <-- Form işlemleri için gerekli
 @Component({
   selector: 'app-login',
   standalone: true, // <-- Standalone bileşen
-  imports: [CommonModule], // <-- *ngIf çalışması için şart
+  imports: [CommonModule, FormsModule], // <-- *ngIf çalışması için şart
   templateUrl: './login.html',
   styleUrls: ['./login.css'] // <-- CSS dosyasını ekliyoruz
 })
 export class Login {
   showPassword = false;
 
+  username = '';
+  password = '';
+
   switchPasswordVisibility() {
     this.showPassword = !this.showPassword;
   }
+  login() {
+    if (!this.username || !this.password) {
+      alert('Lütfen tüm alanları doldurun.');
+      return;
+    }
+    const users = JSON.parse(localStorage.getItem('users') || '[]');
+    const user = users.find((user: any) => (user.email === this.username || user.username === this.username) && user.password === this.password);
+    if (user) {
+      localStorage.setItem('loggedInUser', JSON.stringify(user));
+
+      alert('Giriş başarılı!');
+      // Giriş başarılı olduğunda yapılacak işlemler
+      // Örneğin, kullanıcı bilgilerini saklayabilir veya yönlendirme yapabilirsiniz.
+    } else {
+      alert('Kullanıcı adı veya şifre yanlış.');
+    }
+  }
+
 }
--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -12,7 +12,38 @@ import { FormsModule } from '@angular/forms';
 export class Register {
   showPassword = false;
 
+  email= '';
+  username = '';
+  phone = '';
+  password = '';
+
   switchPasswordVisibility() {
     this.showPassword = !this.showPassword;
+  }
+  register(){
+    if(!this.email || !this.username || !this.phone || !this.password) {
+      alert('Lütfen tüm alanları doldurun.');
+      return;
+    }
+    const users = JSON.parse(localStorage.getItem('users') || '[]');
+    const emailExists = users.some((user: any) => user.email === this.email);
+    const usernameExists = users.some((user: any) => user.username === this.username);
+    if (emailExists) {
+      alert('Bu email adresi zaten kayıtlı.');
+      return;
+    }
+    users.push({
+      email: this.email,
+      username: this.username,
+      phone: this.phone,
+      password: this.password
+    });
+    localStorage.setItem('users', JSON.stringify(users));
+    alert('Kayıt başarılı!');
+    this.email = '';
+    this.username = '';
+    this.phone = '';
+    this.password = '';
+
   }
 }
