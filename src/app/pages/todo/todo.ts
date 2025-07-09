4944929608300f987dd33baf2eda8d8ee609bdbe
Fatma Cicek <fatmacicek139@gmail.com>
2025-07-09 17:28:06 +0300

    sidebar eklendi

--- src/app/pages/todo/todo.ts
+++ src/app/pages/todo/todo.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { Sidebar } from '../../sidebar/sidebar';
 
 @Component({
   selector: 'app-todo',
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterModule, Sidebar],
+  standalone: true,
   templateUrl: './todo.html',
-  styleUrl: './todo.css'
+  styleUrls: ['./todo.css']
 })
 export class Todo {
+
 
 }
